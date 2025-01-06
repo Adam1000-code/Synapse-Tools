014b92b3e9bb73ac7a938e5ca336e85f26c42b0f
Adam1000 <azeiser08@outlook.com>
2025-01-06 09:17:07 -0500

    print beta version

--- version.py
+++ version.py
@@ -4,7 +4,9 @@ from time import sleep
 
 globals.ClearScreen()
 
-print("Synapse Tools version {}.{}, created by {}".format(globals.toolsMajorVer, globals.toolsMinorVer, globals.creator))
+#print("Synapse Tools version {}.{}, created by {}".format(globals.toolsMajorVer, globals.toolsMinorVer, globals.creator))
+
+print("Synapse Tools beta {}.{}, created by {}".format(globals.betaMajorVer, globals.betaMinorVer, globals.creator))
 
 sleep(2)
 
