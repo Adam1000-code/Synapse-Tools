f2822fb40ff1a7b0e3cfa27c1d49f6266c6d4ab8
Adam1000 <azeiser08@outlook.com>
2025-02-06 09:36:16 -0500

    working on advancing project creator

--- project_creator.py
+++ project_creator.py
@@ -1,31 +1,45 @@
 import os
 import globals
 import json
+import subprocess
 from time import sleep
 
 globals.ClearScreen()
 
+print("Use a stable version or nightly build of the engine for the project?")
+print("Nightly version is directly cloned by git from the Synapse Engine repository and has a good chance of containing bugs.")
+print("""1. Stable
+2. Nightly
+Please enter the number 1 or 2 for your selected version""")
+
+engineType = input("> ")
+
+if engineType == "1":
+    print("ERROR: unable to get latest release of the engine")
+elif engineType == 2:
+    subprocess.call(["git", "clone", "https://github.com/Adam1000-code/Synapse-Engine.git"])
+
 print("What would you like your project to be called?")
 
-projName = input(">")
+projName = input("> ")
 
 if projName == "":
     projName = "Default Project"
 
 print("What is the name of the creator?")
 
-creator = input(">")
+creator = input("> ")
 
 if creator == "":
     creator = "Synapse"
 
 print("What should the project ID be? Ex: Synapse-Project")
 
-id = input(">")
+id = input("> ")
 
 print("What version of your game are you starting out with? (Leave blank for 0.1)")
 
-ver = input(">")
+ver = input("> ")
 
 if ver == "":
     ver = 0.1
@@ -56,7 +70,7 @@ with open(idFile, "w") as f:
 
 print("Do you want the main file to be automatically created?")
 
-answer = input("y or n:")
+answer = input("y or n: ")
 
 if answer == "y" or answer == "Y":
     mainFile = os.path.join(srcDir, "main.cpp")
